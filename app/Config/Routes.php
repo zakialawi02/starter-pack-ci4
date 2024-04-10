14828dc66fcaae7f13ed2b55067babf34a017a03
Ahmad zaki Alawi <zaki.alawi22@gmail.com>
2024-04-10 10:13:20 +0700

    simple CRUD

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,8 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->get('/dashboard', 'Dashboard::index');
+$routes->get('/notes', 'Note::index');
+$routes->get('/notes/create', 'Note::create');
+$routes->post('/notes/store', 'Note::store');
---
+++ app/Database/Migrations/2024-04-10-022657_CreateNotesTable.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateNotesTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'title' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'content' => [
+                'type' => 'TEXT',
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('notes');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('notes');
+    }
+}
