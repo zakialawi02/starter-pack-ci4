7c53faaf08cd568f95a3ea819a4e94959051c17a
Ahmad zaki Alawi <zaki.alawi22@gmail.com>
2024-04-15 23:08:21 +0700

    add authentication with myth/auth

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -34,6 +34,9 @@ class Filters extends BaseFilters
         'forcehttps'    => ForceHTTPS::class,
         'pagecache'     => PageCache::class,
         'performance'   => PerformanceMetrics::class,
+        'login'      => \Myth\Auth\Filters\LoginFilter::class,
+        'role'       => \Myth\Auth\Filters\RoleFilter::class,
+        'permission' => \Myth\Auth\Filters\PermissionFilter::class,
     ];
 
     /**
@@ -70,6 +73,7 @@ class Filters extends BaseFilters
     public array $globals = [
         'before' => [
             // 'honeypot',
+            // 'login',
             'csrf',
             // 'invalidchars',
         ],
